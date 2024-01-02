1c769d5c93769fe684efcb79fd506200a9632eb8
JB <drv98@naver.com>
2024-01-02 15:01:36 +0900

    App에 라우팅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Admin from './pages/admin/Admin';
+import NotFound from './pages/not-found/NotFound';
+import UnAuthorized from './pages/unauthorized/UnAuthorized';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="/401" element={<UnAuthorized />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
