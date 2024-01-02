32f869b249fedbb06ff1c2cee7685f193a12bd98
JB <drv98@naver.com>
2024-01-02 14:56:31 +0900

    네브바 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,36 @@ import logo from '../assets/react.svg';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
-      <a href="https://react.dev" className="navbar-brand ms-1">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src="/vite.svg" className="App-logo" alt="logo" />
-        React
-      </a>
+      <div className="container">
+        <a href="https://react.dev" className="navbar-brand ms-1">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src="/vite.svg" className="App-logo" alt="logo" />
+        </a>
+        <div className="navbar-nav me-auto">
+          <li className="nav-item">
+            <a href="##" className="nav-link">
+              관리자
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="##" className="nav-link">
+              홈페이지
+            </a>
+          </li>
+        </div>
+        <div className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <a href="##" className="nav-link">
+              로그인
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="##" className="nav-link">
+              가입하기
+            </a>
+          </li>
+        </div>
+      </div>
     </nav>
   );
 };
