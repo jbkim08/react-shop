1b24e11f8751fea233ae2cde19b855de9b819d69
JB <drv98@naver.com>
2024-01-03 12:25:12 +0900

    관리자 페이지

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import productService from '../../services/Product.service';
 
 const Admin = () => {
-  return <div>관리자페이지</div>;
+  const [productList, setProductList] = useState([]);
+  useEffect(() => {
+    productService.getAllProducts().then((response) => {
+      setProductList(response.data);
+    });
+  }, []);
+  return (
+    <div className="container">
+      <div className="card mt-5">
+        <div className="card-header">
+          <div className="row">
+            <div className="col-6">
+              <h3>모든 제품들</h3>
+            </div>
+
+            <div className="col-6 text-end">
+              <button className="btn btn-primary">새 제품</button>
+            </div>
+          </div>
+        </div>
+        <div className="card-body">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Date</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productList.map((item, index) => (
+                <tr>
+                  <th scope="col">{index + 1}</th>
+                  <td>{item.name}</td>
+                  <td>{item.price} 원</td>
+                  <td>{new Date(item.createTime).toLocaleString()}</td>
+                  <td>
+                    <button className="btn btn-primary me-1">수 정</button>
+                    <button className="btn btn-danger">삭 제</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
--- src/services/Product.service.js
+++ src/services/Product.service.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import { BASE_API_URL } from '../common/constants';
+import { authHeader } from './base.service';
+
 const API_URL = BASE_API_URL + '/api/products';
 
 class ProductService {
