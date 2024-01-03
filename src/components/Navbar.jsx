11aa2e0dfa55e11082f04fa23f18d58d5b263445
JB <drv98@naver.com>
2024-01-03 10:46:09 +0900

    네브바 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import logo from '../assets/react.svg';
+import { clearCurrentUser } from '../store/actions/user';
+import { Role } from '../models/Role';
 
 const Navbar = () => {
+  const currentUser = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    dispatch(clearCurrentUser());
+    navigate('/login');
+  };
+
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
       <div className="container">
@@ -10,29 +23,47 @@ const Navbar = () => {
           <img src="/vite.svg" className="App-logo" alt="logo" />
         </a>
         <div className="navbar-nav me-auto">
-          <li className="nav-item">
-            <NavLink to="/admin" className="nav-link">
-              관리자
-            </NavLink>
-          </li>
+          {currentUser?.role === Role.ADMIN && (
+            <li className="nav-item">
+              <NavLink to="/admin" className="nav-link">
+                관리자
+              </NavLink>
+            </li>
+          )}
           <li className="nav-item">
             <NavLink to="/" className="nav-link">
               홈페이지
             </NavLink>
           </li>
         </div>
-        <div className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <NavLink to="/login" className="nav-link">
-              로그인
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/register" className="nav-link">
-              가입하기
-            </NavLink>
-          </li>
-        </div>
+        {!currentUser && (
+          <div className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink to="/login" className="nav-link">
+                로그인
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/register" className="nav-link">
+                가입하기
+              </NavLink>
+            </li>
+          </div>
+        )}
+        {currentUser && (
+          <div className="navbar-nav ms-auto me-5">
+            <li className="nav-item">
+              <NavLink to="/profile" className="nav-link">
+                {currentUser.name}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <a href="##" className="nav-link" onClick={logout}>
+                로그아웃
+              </a>
+            </li>
+          </div>
+        )}
       </div>
     </nav>
   );
